878d2d1e6c88c0c49e252163e5fc9406a2bf9eec
David Berko <daberko364@gmail.com>
2025-07-31 14:01:44 +0000

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        Circle circle1 = new Circle();
+        circle1.radius = 7;
+        circle1.getArea();
+
+        Rectangle rectangle1 = new Rectangle();
+        rectangle1.height = 4;
+        rectangle1.length = 2;
+        rectangle1.getArea();
+
+
+
+    }
+
+    public abstract class Shape
+    {
+        public abstract void getArea();
+    }
+
+    public class Circle : Shape
+    {
+        public double radius;
+
+        public override void getArea()
+        {
+            double area = 3.142 * (radius * radius);
+            Console.WriteLine(area);
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double length;
+        public double height;
+
+
+        public override void getArea()
+        {
+            double area = height * length;
+            Console.WriteLine(area);
+        }
+
+
+    }
+
+
+
+
+}
