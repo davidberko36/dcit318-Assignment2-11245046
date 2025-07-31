124cc642e0811cd889ce08438540ca17c96f334d
David Berko <daberko364@gmail.com>
2025-07-31 15:11:47 +0000

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+
+        Car car1 = new Car();
+        car1.Move();
+        Bicycle bicycle1 = new Bicycle();
+        bicycle1.Move();
+
+
+    }
+
+
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("The car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("The bicycle is moving");
+        }
+    }
+
+
+
+}
