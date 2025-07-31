2a622b7e0c342399eb42c2b6fa4e98293a54ab50
David Berko <daberko364@gmail.com>
2025-07-31 13:26:58 +0000

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog1 = new Dog();
+        dog1.makeSound();
+        Cat cat1 = new Cat();
+        cat1.makeSound();
+    }
+
+    public class Animal
+    {
+        public virtual void makeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void makeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void makeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+
+
+    
+}
